64bb5c99d0e8698218bfb7b2bbb2e40eeff05277
dth <destotelhorus@users.noreply.github.com>
2020-08-01 23:51:42 +0200

    bugfixes:  - fixes wrong conversion of knots and mps  - fixed clouds AGL from checkWX having been interpreted as ASL before wrongfully.
    new features:
     - implemented --help commandline option
     - implemented option from commandline to just update a single mission
     - implemented option from commandline to suppress date/time changes
     - implemented option from commandline to inject a specific weather to one file or folder from supplied properties file (see sample.weather)
     - implemented option from commandline to copy weather from one MIZ into another.

--- DCSWeatherInjector.py
+++ DCSWeatherInjector.py
@@ -3,7 +3,22 @@ from shutil import copyfile
 from MIZFile import MIZFile
 from CheckWX import CheckWX
 from DCSCheckWXConvertEnricher import DCSCheckWXConvertEnricher
+from DCSWeatherFileConverter import DCSWeatherFileConverter
 import configparser
+import argparse
+
+parser = argparse.ArgumentParser(description='Injects weather into DCS missions.')
+parser.add_argument('missionfile', help='mission file to inject into (default: all files in directory specified '
+                                        'in config.properties). Using this will cause an in-place injection with '
+                                        'the source file overwritten', default=argparse.SUPPRESS, nargs='?')
+parser.add_argument('--weatherfile', help='file with weatherdata to use (default: use real weather in theatre)',
+                    required=False)
+parser.add_argument('--copy-weather-from-miz', help='copies weather from specified existing mission to the target '
+                                                    'mission(s). Doesn''t change time of mission!', required=False)
+parser.add_argument('--dont-update-time', help='Do not update the mission file with current time',
+                    action='store_true', required=False)
+
+args = parser.parse_args()
 
 config = configparser.RawConfigParser()
 config.read('config.properties')
@@ -16,52 +31,83 @@ def MIZFilter(haystack):
     return [str for str in haystack
             if (str.endswith('.miz') and not str.endswith('.realweather.miz'))]
 
+def process_mission(target, weatherdata = None):
+    missionbasename = os.path.splitext(target)[0]
+    MIZ = MIZFile(target, False)
+    print('Date of mission:', MIZ.getDateTime())
+    print('Theatre:', MIZ.getTheatre())
+    if weatherdata == None:
+        weatherprovider = CheckWX(WXDATAAPIKEY)
+        weatherdata = DCSCheckWXConvertEnricher(weatherprovider.getWeatherForLatLon(MIZ.getTheatreLatLon()['lat'],
+                                                                                    MIZ.getTheatreLatLon()['lon']))
+    print('Weather for mission:', weatherdata.getWeatherText())
+    print('Weather was cached:', weatherdata.getWeatherCached())
+    if not '.fixedtime.' in missionbasename and not args.dont_update_time:
+        MIZ.setDateTimeNow()
+    print('Date of mission (new):', MIZ.getDateTime())
+    weather = MIZ.getWeather()
+    weather['atmosphere_type'] = 0 #Setting static weather
+    weather['cyclones'] = []
+    weather['season']['temperature'] = weatherdata.getTemperatureASL()
+    windASL = weatherdata.getWindASL()
+    wind2000 = weatherdata.getWind2000()
+    wind8000 = weatherdata.getWind8000()
+    weather['wind']['atGround']['speed'] = windASL['speed']
+    weather['wind']['atGround']['dir'] = windASL['direction']
+    weather['wind']['at2000']['speed'] = wind2000['speed']
+    weather['wind']['at2000']['dir'] = wind2000['direction']
+    weather['wind']['at8000']['speed'] = wind8000['speed']
+    weather['wind']['at8000']['dir'] = wind8000['direction']
+    weather['enable_fog'] = weatherdata.getFogEnabled()
+    weather['qnh'] = weatherdata.getBarometerMMHg()
+    weather['dust_density'] = 0
+    weather['enable_dust'] = False
+    weather['clouds']['density'] = weatherdata.getCloudDensity()
+    weather['clouds']['thickness'] = weatherdata.getCloudThickness()
+    weather['clouds']['base'] = weatherdata.getCloudBase()
+    weather['clouds']['iprecptns'] = weatherdata.getWeatherType()
+    weather['groundTurbulence'] = weatherdata.getGroundTurbulence()
+    weather['type_weather'] = 0
+    weather['fog']['thickness'] = weatherdata.getFogThickness()
+    weather['fog']['visibility'] = weatherdata.getFogVisibility()
+    weather['visibility']['distance'] = weatherdata.getVisibility()
+
+    MIZ.setWeather(weather)
+    MIZ.commit()
+    print('Saved', target, 'successfully.')
+
+def copy_weather(source, target):
+    print('Copying weather from ', source,' to ', target, '.')
+    TargetMIZ = MIZFile(target, False)
+    SourceMIZ = MIZFile(source, True)
+    TargetMIZ.setWeather(SourceMIZ.getWeather())
+    TargetMIZ.commit()
+    print('Saved', target, 'successfully.')
 
 if __name__ == '__main__':
-    missions = MIZFilter(os.listdir(missiondir))
-    weatherprovider = CheckWX(WXDATAAPIKEY)
+    if args.copy_weather_from_miz:
+        if not hasattr(args, 'missionfile'):
+            missions = MIZFilter(os.listdir(missiondir))
 
-    print('Processing missions:', missions)
-    for mission in missions:
-        missionbasename = os.path.splitext(mission)[0]
-        print('> ', mission)
-        copyfile(missiondir+'/'+mission, targetdir+'/'+missionbasename+'.realweather.miz')
-        MIZ = MIZFile(targetdir+'/'+missionbasename+'.realweather.miz', False)
-        print('Date of mission:', MIZ.getDateTime())
-        print('Theatre:', MIZ.getTheatre())
-        weatherdata = DCSCheckWXConvertEnricher(weatherprovider.getWeatherForLatLon(MIZ.getTheatreLatLon()['lat'], MIZ.getTheatreLatLon()['lon']))
-        print('Weather for mission:', weatherdata.getLastWeather().text)
-        print('Weather was cached:', weatherdata.getLastWeather().from_cache)
-        if not '.fixedtime.' in missionbasename:
-            MIZ.setDateTimeNow()
-        print('Date of mission (new):', MIZ.getDateTime())
-        weather = MIZ.getWeather()
-        weather['atmosphere_type'] = 0 #Setting static weather
-        weather['cyclones'] = []
-        weather['season']['temperature'] = weatherdata.getTemperatureASL()
-        windASL = weatherdata.getWindASL()
-        wind2000 = weatherdata.getWind2000()
-        wind8000 = weatherdata.getWind8000()
-        weather['wind']['atGround']['speed'] = windASL['speed']
-        weather['wind']['atGround']['dir'] = windASL['direction']
-        weather['wind']['at2000']['speed'] = wind2000['speed']
-        weather['wind']['at2000']['dir'] = wind2000['direction']
-        weather['wind']['at8000']['speed'] = wind8000['speed']
-        weather['wind']['at8000']['dir'] = wind8000['direction']
-        weather['enable_fog'] = weatherdata.getFogEnabled()
-        weather['qnh'] = weatherdata.getBarometerMMHg()
-        weather['dust_density'] = 0
-        weather['enable_dust'] = False
-        weather['clouds']['density'] = weatherdata.getCloudDensity()
-        weather['clouds']['thickness'] = weatherdata.getCloudThickness()
-        weather['clouds']['base'] = weatherdata.getCloudBase()
-        weather['clouds']['iprecptns'] = weatherdata.getWeatherType()
-        weather['groundTurbulence'] = weatherdata.getGroundTurbulence()
-        weather['type_weather'] = 0
-        weather['fog']['thickness'] = weatherdata.getFogThickness()
-        weather['fog']['visibility'] = weatherdata.getFogVisibility()
-        weather['visibility']['distance'] = weatherdata.getVisibility()
+            print('Processing missions:', missions)
+            for mission in missions:
+                missionbasename = os.path.splitext(mission)[0]
+                print('> ', mission)
+                copyfile(missiondir+'/'+mission, targetdir+'/'+missionbasename+'.realweather.miz')
+                copy_weather(args.copy_weather_from_miz, targetdir+'/'+missionbasename+'.realweather.miz')
+        else:
+            copy_weather(args.copy_weather_from_miz, args.missionfile)
+    else:
+        if not hasattr(args, 'missionfile'):
+            missions = MIZFilter(os.listdir(missiondir))
 
-        MIZ.setWeather(weather)
-        MIZ.commit()
-        print('Saved', mission, 'successfully.')
+            print('Processing missions:', missions)
+            for mission in missions:
+                missionbasename = os.path.splitext(mission)[0]
+                print('> ', mission)
+                copyfile(missiondir+'/'+mission, targetdir+'/'+missionbasename+'.realweather.miz')
+                process_mission(targetdir+'/'+missionbasename+'.realweather.miz')
+        else:
+            weatherdata = DCSWeatherFileConverter(args.weatherfile)
+            process_mission(args.missionfile, weatherdata)
+
--- README.md
+++ README.md
@@ -11,7 +11,33 @@
 `pip install -r requirements.txt`
 
 ## Run
-`python DCSWeatherInjector.py`
+`python DCSWeatherInjector.py --help`
+
+## Help
+```
+usage: DCSWeatherInjector.py [-h] [--weatherfile WEATHERFILE]
+                              [--copy-weather-from-miz COPY_WEATHER_FROM_MIZ]
+                              [--dont-update-time]
+                              [missionfile]
+ 
+ Injects weather into DCS missions.
+ 
+ positional arguments:
+   missionfile           mission file to inject into (default: all files in
+                         directory specified in config.properties). Using this
+                         will cause an in-place injection with the source file
+                         overwritten
+ 
+ optional arguments:
+   -h, --help            show this help message and exit
+   --weatherfile WEATHERFILE
+                         file with weatherdata to use (default: use real
+                         weather in theatre)
+   --copy-weather-from-miz COPY_WEATHER_FROM_MIZ
+                         copies weather from specified existing mission to the
+                         target mission(s). Doesnt change time of mission!
+   --dont-update-time    Do not update the mission file with current time
+```
 
 ## What will it do
 It will copy every `*.miz` (excluding `*.realweather.miz`) from the source directory into the target directory under the name `*.realweather.miz` and inject weather and time information into it.
